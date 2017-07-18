4d1a585aa6b54c71281124538f0965b5a25f211e
Ross Goodwin <ross.goodwin@gmail.com>
2017-07-18 16:44:03 -0400

    spaces > tabs

--- code/scrape.py
+++ code/scrape.py
@@ -8,35 +8,35 @@ page_nums = range(700)[::120]
 entry_urls = list()
 
 for num in page_nums:
-	payload = {'s': str(num)}
-	res = requests.get(endpt, params=payload)
-	soup = BeautifulSoup(res.text)
-	tgt_links = soup.select('a.result-title')
+    payload = {'s': str(num)}
+    res = requests.get(endpt, params=payload)
+    soup = BeautifulSoup(res.text)
+    tgt_links = soup.select('a.result-title')
 
-	for link in tgt_links:
-		entry_urls.append(link['href'])
+    for link in tgt_links:
+        entry_urls.append(link['href'])
 
 
 # print entry_urls
 
 i = 0
 for url in entry_urls:
-	url = 'https://newyork.craigslist.org' + url
-	res = requests.get(url)
-	soup = BeautifulSoup(res.text)
-	try:
-		tgt_section = soup.select('#postingbody')[0]
-	except IndexError:
-		pass
-	else:
-		tgt_text = tgt_section.get_text()
+    url = 'https://newyork.craigslist.org' + url
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text)
+    try:
+        tgt_section = soup.select('#postingbody')[0]
+    except IndexError:
+        pass
+    else:
+        tgt_text = tgt_section.get_text()
 
-		# print tgt_text
-		# break
+        # print tgt_text
+        # break
 
-		final_text = tgt_text.replace('QR Code Link to This Post', '').strip()
+        final_text = tgt_text.replace('QR Code Link to This Post', '').strip()
 
-		with open('data/%i.txt' % i, 'w') as outfile:
-			outfile.write(final_text.encode("utf8"))
+        with open('data/%i.txt' % i, 'w') as outfile:
+            outfile.write(final_text.encode("utf8"))
 
-	i += 1
+    i += 1
